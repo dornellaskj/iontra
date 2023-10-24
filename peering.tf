3083eb8e2337b14e5be2b335e5d1b3598370e002
Kevin D'Ornellas <dornellaskj@gmail.com>
2023-10-24 10:59:12 -0600

    adding NACL rules to allow only 80/443 access from VPC-A public subnet

--- peering.tf
+++ peering.tf
@@ -10,3 +10,47 @@ resource "aws_route" "vpc_a_public_to_vpc_b_private" {
   destination_cidr_block    = element(var.vpc_b_private_cidrs, count.index)
   vpc_peering_connection_id = aws_vpc_peering_connection.A2B.id
 }
+
+resource "aws_network_acl_rule" "VPCa_ingress" {
+  network_acl_id = module.vpc-B.vpc_id.default_network_acl_id
+  count       = length(var.vpc_a_public_cidrs)
+  rule_number = "${5 + count.index}"
+  egress      = false
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = element(var.vpc_a_public_cidrs, count.index)
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_network_acl_rule" "VPCa_egress" {
+  network_acl_id = module.vpc-B.vpc_id.default_network_acl_id
+  count       = length(var.vpc_a_public_cidrs)
+  rule_number = "${5 + count.index}"
+  egress      = true
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = element(var.vpc_a_public_cidrs, count.index)
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_network_acl_rule" "VPCa_ingress_deny" {
+  network_acl_id = module.vpc-B.vpc_id.default_network_acl_id
+  count       = length(var.vpc_a_public_cidrs)
+  rule_number = "${1000 + count.index}"
+  egress      = false
+  protocol    = -1
+  rule_action = "deny"
+  cidr_block  = element(var.vpc_a_public_cidrs, count.index)
+}
+
+resource "aws_network_acl_rule" "VPCa_egress_deny" {
+  network_acl_id = module.vpc-B.vpc_id.default_network_acl_id
+  count       = length(var.vpc_a_public_cidrs)
+  rule_number = "${1000 + count.index}"
+  egress      = true
+  protocol    = -1
+  rule_action = "deny"
+  cidr_block  = element(var.vpc_a_public_cidrs, count.index)
+}
